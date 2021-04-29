246318083bc6e6c832f738e9846ebb5d1e465a8d
GeoffMahugu <geoffreymahugu@gmail.com>
2021-04-29 15:50:46 +0300

    Axios init

--- src/components/github_user.js
+++ src/components/github_user.js
@@ -1,13 +1,34 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
+import { debounce } from 'lodash';
+import axios from 'axios';
+
+const GITHUB_API = 'https://api.github.com/users/';
 
 const GitHubUser = () => {
     const [username, setUsername] = useState('GeoffMahugu');
-    // useEffect(()=>{
-    //     setUsername();
-    // },[username]);
+    useEffect(() => {
+        console.log('USEEFFECT', username);
+        debounce(() => fetchApi(username), 500);
+        // debounce(() => {
+        //     console.log('PRE::::', username);
+        //     fetchApi(username);
+        // }, 500);
+    }, [username]);
     const handleInput = (event) => {
         setUsername(event.target.value);
-    }
+    };
+    useCallback(() => {
+        fetchApi(username);
+    }, [username]);
+
+    const fetchApi = (searchStr) => {
+        // debounce(() => {
+        //     console.log('SEARCH ITEM::', searchStr);
+        // }, 500);
+
+        console.log('SEARCH ITEM::', searchStr);
+    };
+
     return (
         <>
             <h4>Fetch User profile</h4>
