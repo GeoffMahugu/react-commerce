70fcc3f01e996af3d497c5de725e01a9b1b28f12
GeoffMahugu <geoffreymahugu@gmail.com>
2021-04-29 19:05:43 +0300

    Fetch user

--- src/components/github_user.js
+++ src/components/github_user.js
@@ -1,40 +1,48 @@
-import React, { useEffect, useCallback, useState } from 'react';
+import React, { useEffect, useCallback, useState, useRef } from 'react';
 import { debounce } from 'lodash';
 import axios from 'axios';
 
 const GITHUB_API = 'https://api.github.com/users/';
 
 const GitHubUser = () => {
-    const [username, setUsername] = useState('GeoffMahugu');
+    const [username, setUserName] = useState('');
+    const [userdata, setUserData] = useState('');
+    const fetchGitUser = useRef(debounce(() => { console.log('FETCHING', username); fetchApi(username); }, 2000)).current;
+
     useEffect(() => {
-        console.log('USEEFFECT', username);
-        debounce(() => fetchApi(username), 500);
-        // debounce(() => {
-        //     console.log('PRE::::', username);
-        //     fetchApi(username);
-        // }, 500);
+        fetchGitUser();
     }, [username]);
+
+
     const handleInput = (event) => {
-        setUsername(event.target.value);
+        setUserName(event.target.value);
     };
-    useCallback(() => {
-        fetchApi(username);
-    }, [username]);
 
-    const fetchApi = (searchStr) => {
-        // debounce(() => {
-        //     console.log('SEARCH ITEM::', searchStr);
-        // }, 500);
-
-        console.log('SEARCH ITEM::', searchStr);
+    const fetchApi = async () => {
+        // console.log('::::::::::::::::::::::');
+        // console.log('SearchSTR', searchStr);
+        // console.log('USERNAME', username);
+        if (!username) return;
+        try {
+            const response = await axios.get(`${GITHUB_API}${username}`);
+            const data = await response.data;
+            setUserData(data);
+            console.log('RESPONSE::', data);
+        } catch (e) {
+            console.log('ERROR::', e);
+        }
     };
 
     return (
         <>
             <h4>Fetch User profile</h4>
-            <input type="text" value={username} onChange={handleInput} />
             <div>
-                <p>UserName: {username}</p>
+                <label htmlFor="search-git-user">Search GitHub User </label>
+                <input type="text" id="search-git-user" value={username} onChange={handleInput} />
+            </div>
+            <div>{username}</div>
+            <div>
+                {userdata ? <div><pre>{userdata}</pre></div> : <p>No user data to display</p>}
             </div>
         </>
     )
